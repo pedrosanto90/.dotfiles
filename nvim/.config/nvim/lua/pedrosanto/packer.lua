d6455a57ea9966144d1c5f1aa42c15aa11b695ac
pedrosanto90 <pedro.bb.90@gmail.com>
2024-01-13 15:14:12 +0000

    add vim-emmet

--- nvim/.config/nvim/lua/pedrosanto/packer.lua
+++ nvim/.config/nvim/lua/pedrosanto/packer.lua
@@ -5,24 +5,24 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  --telescope
-  use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
--- Tokyo Night
- use ({
-	'gruvbox-community/gruvbox',
-	as = 'tokyonight',
-	config = function()
+    --telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    -- Tokyo Night
+    use ({
+        'gruvbox-community/gruvbox',
+        as = 'tokyonight',
+        config = function()
             vim.cmd('colorscheme gruvbox')
 
-	end
-})
+        end
+    })
 
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use ('ThePrimeagen/harpoon')
@@ -31,47 +31,45 @@ return require('packer').startup(function(use)
 
     --LSP
     use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    --- Uncomment these if you want to manage LSP servers from neovim
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-  }
-}
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+    -- autopairs
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
-use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-}
+    -- lua line
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
--- lua line
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
+    -- VimBeGood
+    use {
+        'ThePrimeagen/vim-be-good'
+    }
 
--- VimBeGood
-use {
-    'ThePrimeagen/vim-be-good'
-}
-
--- Vim commentary
-use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-}
--- Assembly Parser
-use { 'rush-rs/tree-sitter-asm' }
-
-
+    -- Vim commentary
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    -- emmet
+    use 'mattn/emmet-vim'
 end)
