56514b7266f0ca450c8b9529f787aa5c113422c9
pedrosanto90 <pedro.bb.90@gmail.com>
2024-10-02 20:02:04 +0100

    change background

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,8 +16,10 @@ config.colors = {
     cursor_border = '#ffffff',
 
 }
+config.window_background_opacity = 0.90
 config.default_cursor_style = 'BlinkingBlock'
 config.cursor_blink_rate = 700
+config.hide_tab_bar_if_only_one_tab = true
 -- and finally, return the configuration to wezterm
-config.hide_mouse_cursor_when_typing = false
+config.hide_mouse_cursor_when_typing = true
 return config
