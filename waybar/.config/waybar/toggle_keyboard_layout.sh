984d9f742ba0282fe510e3452156609e19bb6132
pedrosanto90 <pedro.bb.90@gmail.com>
2025-05-11 17:13:57 +0100

    ...

--- waybar/.config/waybar/toggle_keyboard_layout.sh
+++ waybar/.config/waybar/toggle_keyboard_layout.sh
@@ -2,7 +2,7 @@
 
 # Define your layouts
 LAYOUT1="us"
-LAYOUT2="pt"
+LAYOUT2="br"
 
 # Get the current layout
 CURRENT_LAYOUT=$(swaymsg -t get_inputs | grep -m1 'xkb_active_layout_name' | cut -d '"' -f4)
