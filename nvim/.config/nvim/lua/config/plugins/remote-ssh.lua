0e0bc771201745508e21a9902a0f12a4b80550ba
Pedro Santo <pedro.bb.90@gmail.com>
2026-04-06 13:21:27 +0100

    feat/added keymaps to remote-ssh.lua

--- nvim/.config/nvim/lua/config/plugins/remote-ssh.lua
+++ nvim/.config/nvim/lua/config/plugins/remote-ssh.lua
@@ -25,4 +25,11 @@ return {
     },
     log = { enabled = false },
   },
+  config = function(_, opts)
+    require('remote-sshfs').setup(opts)
+    local api = require('remote-sshfs.api')
+    vim.keymap.set('n', '<leader>rc', api.connect, {})
+    vim.keymap.set('n', '<leader>rd', api.disconnect, {})
+    vim.keymap.set('n', '<leader>re', api.edit, {})
+  end,
 }
